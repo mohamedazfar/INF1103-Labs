3324e9c18490cae9aa6c5b76ba9a3c86946d6207
mohamedazfar <mohamedazfar007@gmail.com>
2026-09-11 11:39:33 +0800

    Handle invalid input

--- auditor.py
+++ auditor.py
@@ -2,5 +2,12 @@ inventory = 0
 
 while True:
     stock = input("Enter a stock quantity: ")
-    if stock == 'quit':
+    
+    if stock.isdigit():
+        stock = int(stock)
+
+    elif stock == 'quit':
         break
+
+    else:
+        print("Error! Enter only positive integer")
