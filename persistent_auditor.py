c5c8bb91376559d95d70ba9d57f71a743a4753db
mohamedazfar <mohamedazfar007@gmail.com>
2026-09-25 22:24:06 +0800

    Complete Lab 4

--- persistent_auditor.py
+++ persistent_auditor.py
@@ -1,24 +1,26 @@
 import os
 
 def load_inventory(file_name):
-
     if not os.path.isfile(file_name):
         with open(file_name, "w") as file:
-            print("\nNo Current current_orders found.")
-        return None
+            print("\nNo Current Orders found.")
+        return None, 0
             
     else:
         with open(file_name, "r") as file:
             current_orders = file.readlines()
 
+        inventory = int(current_orders[-1].split(":")[1].strip())
+        del current_orders[-1]
+
         print("\nCurrent Orders:")
         for order in current_orders:
             print(order, end="")
-        return current_orders
+
+        return current_orders, inventory
 
 def get_valid_input():
-    print("\n")
-    productName = input("Enter Product Name: ")
+    productName = input("\nEnter Product Name: ")
     if productName == 'quit':
         return productName
 
@@ -27,7 +29,7 @@ def get_valid_input():
         return quantity
     
     elif quantity.isdigit():
-        return productName, int(quantity)
+        return productName, quantity
     
     else:
         print("Error! Enter a non-negative integer.")
@@ -38,24 +40,26 @@ def getNewID(orderIDLatest):
     return str(new_id)
 
 def addToNewOrders(new_id, order, new_orders):
-    transaction =  new_id + ", " + order[0] + ", " + str(order[1]) + "\n"
-
+    transaction =  new_id + ", " + order[0] + ", " + order[1] + "\n"
     new_orders.append(transaction)
     print("\nNew Order Added:")
     print(transaction)
     return new_orders
 
-def save_inventory(file_name, new_orders):
-    with open(file_name, "a") as file:
+def save_inventory(file_name, current_orders, new_orders, inventory):
+    with open(file_name, "w") as file:
+        if current_orders is not None:
+            file.writelines(current_orders)
         file.writelines(new_orders)
-    print(f"Order successfully saved to {file_name}")
+        file.write(f"Inventory Total: {inventory}\n")
+    print(f"\nOrder successfully saved to {file_name}\n")
 
 def getLatestOrderID(list):
     id = max(list).split(",")[0]
     return id
 
 def calculate_total(inventory, order):
-    inventory += order[1]
+    inventory += int(order[1])
     return inventory
 
 # Main program
@@ -64,23 +68,23 @@ inventory = 0
 new_orders = []
 orderID_start = 1000
 
+current_orders, inventory = load_inventory(file_name)
 
-current_orders = load_inventory(file_name)
-orderIDLatest = getLatestOrderID(current_orders)
+if current_orders is None:
+    orderIDLatest = orderID_start
+else:
+    orderIDLatest = getLatestOrderID(current_orders)
 
 while True:
     order = get_valid_input()
     if order == 'quit':
         break
-
     if order is None:
         continue
 
     new_id = getNewID(orderIDLatest)
     new_orders = addToNewOrders(new_id, order, new_orders)
-
     orderIDLatest = getLatestOrderID(new_orders)
     inventory = calculate_total(inventory, order)
     
-print(inventory)
-save_inventory(file_name, new_orders)
+save_inventory(file_name, current_orders, new_orders, inventory)
